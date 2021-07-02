472302230b660dc82141e6bc3ed5a9d1bb562c39
Patrick Coleman <pcolemanofficial@gmail.com>
2021-07-01 20:35:21 -0400

    fixed run environment variables being labeled wrong

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app.utils.conf_parse import get_config
 
 if __name__=='__main__':
     c = get_config()
-    app.run(debug=False, host=c['SUI_HOST'], port=c['SUI_PORT'])
+    app.run(debug=False, host=c['SBO_HOST'], port=c['SBO_PORT'])
 
